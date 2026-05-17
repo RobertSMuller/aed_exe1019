7d8f242f7a13fb182d5ddecd9d9a7da72857639e
RobertSMuller <137554584+RobertSMuller@users.noreply.github.com>
2026-05-17 19:18:34 -0300

    Add files via upload

---
+++ test_1019.c
@@ -0,0 +1,39 @@
+int* nextLargerNodes(struct ListNode* head, int* returnSize) {
+    int valor = 0, tamanho = 0;
+    struct ListNode *ptr = head ;
+    struct ListNode *ptr2;
+    
+     ptr=head;
+
+    while(ptr != NULL){
+        ptr = ptr->next;
+        tamanho++;
+    }
+   
+    *returnSize = tamanho;
+    int *arrey = (int *)malloc(sizeof(int)*tamanho);
+    
+    tamanho = 0;
+
+     while(ptr != NULL){
+        valor=ptr->val;
+        ptr2=ptr->next;
+
+        while(ptr2 != NULL){
+            if(valor < ptr2->val){
+                valor= ptr2->val;
+                break;
+            }
+            ptr2 = ptr2->next;
+        }
+        if(valor == ptr->val){
+            valor = 0;
+        }
+
+    arrey[tamanho] = valor;
+    tamanho++;
+    ptr= ptr->next;
+     }
+
+return arrey;
+}
